712be47427d4d1a7819cab2cfe7832f920967b39
Enzo-Chantereault <enzo.chantereault@efrei.net>
2024-01-13 10:57:01 +0100

    feat(OracleLib): failsafe if chainlink pricefeed fails

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -30,8 +30,9 @@ import {console} from "forge-std/Test.sol";
 
 import {ReentrancyGuard} from "@openzeppelin/security/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
-import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 import {AggregatorV3Interface} from "@chainlink/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {OracleLib} from "./libraries/OracleLib.sol";
 
 /**
  * @title DSCEngine
@@ -70,6 +71,11 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__MintFailed();
     error DSCEngine__HealthFactorOk();
     error DSCEngine__HealthFactorNotImproved();
+
+    ////////////
+    // TYPES //
+    //////////
+    using OracleLib for AggregatorV3Interface;
 
     //////////////////////
     // STATE VARIABLES //
@@ -281,7 +287,7 @@ contract DSCEngine is ReentrancyGuard {
     /////////////////////////////////////
     function getTokenAmountFromUsd(address token, uint256 usdAmountInWei) public view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
-        (, int256 price,,,) = priceFeed.latestRoundData();
+        (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
 
         return (usdAmountInWei * PRECISION) / (uint256(price) * ADDITIONAL_FEED_PRECISION);
     }
@@ -299,7 +305,7 @@ contract DSCEngine is ReentrancyGuard {
 
     function getUsdValue(address token, uint256 amount) public view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
-        (, int256 price,,,) = priceFeed.latestRoundData();
+        (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
 
         /* OPERATION BELOW EXPLAINED
         * int256 and uint256 don't have decimals, they are integers. Therefore we add 0 behind the integer part
